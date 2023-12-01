0c9991c7b7f47b8a582aa763a4f8e46ff6afa1c6
Overlord7667 <aspiaro@gmail.com>
2023-12-01 21:59:20 +0300

    add layouts and classes

--- app/src/main/java/com/betelgeuse/corp/papajohns/MainActivity.java
+++ app/src/main/java/com/betelgeuse/corp/papajohns/MainActivity.java
@@ -2,8 +2,12 @@ package com.betelgeuse.corp.papajohns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        RelativeLayout pizzas = findViewById(R.id.pizzasID);
+        RelativeLayout pastas = findViewById(R.id.pastaID);
+
+
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public void pizzasShow(View view) {
+        Toast.makeText(this, "PIZZA", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, PizzaActivity.class);
+        this.setTitle("Pizzas");
+        startActivity(intent);
+    }
+
+    public void pastasShow(View view) {
+        Toast.makeText(this, "PASTA", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, PastaActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/betelgeuse/corp/papajohns/PizzaActivity.java
+++ app/src/main/java/com/betelgeuse/corp/papajohns/PizzaActivity.java
@@ -3,12 +3,15 @@ package com.betelgeuse.corp.papajohns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
-public class ProductActivity extends AppCompatActivity {
+public class PizzaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        setContentView(R.layout.activity_pizza);
+
+        this.setTitle("Pizzas");
     }
 }
