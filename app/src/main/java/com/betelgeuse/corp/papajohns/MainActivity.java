03d6e660a0d324c123fb6eefe42a2d4418445360
Overlord7667 <aspiaro@gmail.com>
2023-12-08 21:29:21 +0300

    add Lists Pizza and Pasta

--- app/src/main/java/com/betelgeuse/corp/papajohns/MainActivity.java
+++ app/src/main/java/com/betelgeuse/corp/papajohns/MainActivity.java
@@ -1,44 +1,78 @@
 package com.betelgeuse.corp.papajohns;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> myList = new ArrayList<>();
+    ArrayList<String> AllPizz = new ArrayList<>();
+    ArrayList<String> AllPasta = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout pizzas = findViewById(R.id.pizzasID);
-        RelativeLayout pastas = findViewById(R.id.pastaID);
+        myList.add("Элемент1");
+        myList.add("Элемент2");
+        myList.add("Элемент3");
 
+        AllPizz.add(("Пицца с пиццей"));
+        AllPizz.add(("Пицца с тестом"));
+        AllPizz.add(("Пицца с фигом"));
+        AllPizz.add(("Пицца с чем-то"));
 
+        AllPasta.add(("Паста с пастой"));
+        AllPasta.add(("Паста с лапшой"));
+        AllPasta.add(("Лапша с пастой"));
+        AllPasta.add(("Паста с ушей"));
 
+//        if (AllPizz != null) {
+//            String text = AllPizz.toString();
+//            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+//            // Дальнейшая обработка текста
+//        } else {
+//            // Обработка ошибки или сообщение пользователю
+//            Toast.makeText(this, "DAVAI PO NOVOJ", Toast.LENGTH_SHORT).show();
+//        }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
 
     public void pizzasShow(View view) {
-        Toast.makeText(this, "PIZZA", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, PizzaActivity.class);
-        this.setTitle("Pizzas");
+        Intent intent = new Intent(this, PizzasActivity.class);
+        intent.putExtra("key", AllPizz.get(0));
+        intent.putExtra("key1", AllPizz.get(1));
+        intent.putExtra("key2", AllPizz.get(2));
+        intent.putExtra("key3", AllPizz.get(3));
+        intent.putExtra("title", getTitle());
+        setResult(0,intent);
         startActivity(intent);
     }
 
+
+
     public void pastasShow(View view) {
-        Toast.makeText(this, "PASTA", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, PastaActivity.class);
+        intent.putExtra("key", AllPasta.get(0));
+        intent.putExtra("key1", AllPasta.get(1));
+        intent.putExtra("key2", AllPasta.get(2));
+        intent.putExtra("key3", AllPasta.get(3));
+        setResult(1,intent);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/betelgeuse/corp/papajohns/PizzasActivity.java
+++ app/src/main/java/com/betelgeuse/corp/papajohns/PizzasActivity.java
@@ -1,13 +1,19 @@
 package com.betelgeuse.corp.papajohns;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.icu.text.CaseMap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -17,50 +23,67 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PastaActivity extends AppCompatActivity {
-    List<Pasta> AllPasta = new ArrayList<>();
+public class PizzasActivity extends AppCompatActivity {
+    List<Pizza> AllPizzas = new ArrayList<>();
     Intent intent;
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        intent = data;
+//
+//        if (data != null)
+//        {
+//            Toast.makeText(this, "NEWPIZZA", Toast.LENGTH_SHORT).show();
+////            String addPizza = intent.getStringExtra("key");
+////            AllPizzas.add(new Pizza(addPizza, R.drawable.picca));
+//        }else {
+//            Toast.makeText(this, "NOT DATA", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pasta);
-        this.setTitle("Pasta");
+        setContentView(R.layout.activity_pizzas);
+        setTitle("Pizzas");
 
         intent = getIntent();
 
-        ListView listView = findViewById(R.id.listPasta);
+        ListView listView = findViewById(R.id.listPizzas);
 //        AllPizzas.add(new Pizza("N1", R.drawable.picca));
 //        AllPizzas.add(new Pizza("N2", R.drawable.picca));
 //        AllPizzas.add(new Pizza("N3", R.drawable.picca));
 
-        String addPizza = intent.getStringExtra("key");
-        String addPizza1 = intent.getStringExtra("key1");
-        String addPizza2 = intent.getStringExtra("key2");
-        String addPizza3 = intent.getStringExtra("key3");
-        AllPasta.add(new PastaActivity.Pasta(addPizza, R.drawable.italyanskaya_pasta));
-        AllPasta.add(new PastaActivity.Pasta(addPizza1, R.drawable.italyanskaya_pasta));
-        AllPasta.add(new PastaActivity.Pasta(addPizza2, R.drawable.italyanskaya_pasta));
-        AllPasta.add(new PastaActivity.Pasta(addPizza3, R.drawable.italyanskaya_pasta));
+            String addPizza = intent.getStringExtra("key");
+            String addPizza1 = intent.getStringExtra("key1");
+            String addPizza2 = intent.getStringExtra("key2");
+            String addPizza3 = intent.getStringExtra("key3");
+
+            AllPizzas.add(new Pizza(addPizza, R.drawable.picca));
+            AllPizzas.add(new Pizza(addPizza1, R.drawable.picca));
+            AllPizzas.add(new Pizza(addPizza2, R.drawable.picca));
+            AllPizzas.add(new Pizza(addPizza3, R.drawable.picca));
 
 
 //        ArrayList<String> myList = (ArrayList<String>) intent.getSerializableExtra("myList");
 //        // Создайте адаптер для списка:
 //        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myList);
 
-        PastaActivity.PastaAdapter pizzaAdapter = new PastaActivity.PastaAdapter(AllPasta, this, R.layout.card_products);
+        PizzaAdapter pizzaAdapter = new PizzaAdapter(AllPizzas, this, R.layout.card_products);
         // Установите адаптер для списка:
         listView.setAdapter(pizzaAdapter);
-
     }
-    class PastaAdapter extends BaseAdapter {
 
-        List<Pasta> Pastas;
+    class PizzaAdapter extends BaseAdapter{
+
+        List<Pizza> Pizzas;
         Context context;
         int TemplateLayout;
         LayoutInflater inflater;
 
-        public PastaAdapter(List<PastaActivity.Pasta> pastas, Context context, int templateLayout) {
-            Pastas = pastas;
+        public PizzaAdapter(List<Pizza> pizzas, Context context, int templateLayout) {
+            Pizzas = pizzas;
             this.context = context;
             TemplateLayout = templateLayout;
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -68,12 +91,12 @@ public class PastaActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return Pastas.size();
+            return Pizzas.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return Pastas.get(position);
+            return Pizzas.get(position);
         }
 
         @Override
@@ -86,22 +109,22 @@ public class PastaActivity extends AppCompatActivity {
             View view = inflater.inflate(TemplateLayout, parent, false);
             TextView name = view.findViewById(R.id.text_cardsID);
             ImageView photo = view.findViewById(R.id.imgPizza);
-            Pasta pasta = Pastas.get(position);
-            name.setText(pasta.getName());
-            photo.setImageResource(pasta.getImageId());
+            Pizza pizza = Pizzas.get(position);
+            name.setText(pizza.getName());
+            photo.setImageResource(pizza.getImageId());
             return view;
         }
     }
 
-    class Pasta{
+    class Pizza{
         String Name;
         int ImageId;
 
-        public Pasta(String name, int imageId) {
+        public Pizza(String name, int imageId) {
             Name = name;
             ImageId = imageId;
         }
-        public Pasta(){}
+        public Pizza(){}
 
         public String getName() {
             return Name;
